37eaf7c1ff77b51ed2763ae2ee01a0028aba10de
Noah <64222857+BlaNKtext@users.noreply.github.com>
2020-05-11 19:39:41 -0400

    Update start.js

--- start.js
+++ start.js
@@ -10,7 +10,7 @@ if (process.platform === "win32") {
 }
 
 process.on("SIGINT", function () {
-  console.log('\nChecked ' +i+' codes!\nFound '+a+' working codes haha, get better codes shithead!')
+  console.log('\nChecked ' +i+' codes!\nFound '+a+' working codes!')
   process.exit();
 });
 
@@ -21,7 +21,7 @@ const Agent = require('socks5-http-client/lib/Agent');
 //Options
 const codes = "codes.txt"; //codes.txt and proxies.txt is just an example, put the filename of where your codes are stored.
 const proxies = "proxies.txt"; //proxies.txt can be http proxies or socks5 proxies! Remember, if you are using socks5, change const socks5 = false to const socks5 = true
-const speed = 20 //Measured in milliseconds
+const speed = 200 //Measured in milliseconds
 const debug = false //Debug Mode: Set to true or false
 let socks5 = false //Using Socks5 Proxies (force turned off if autograbproxy is on): Set to true or false
 const autogen = true //Generates codes and checks them: Set to true or false
@@ -45,7 +45,7 @@ if (autograbproxy) {
 });
 }
 
-console.log(colors.green(`\n-------------------------------------\n[CHECKER] Discord Nitro Code Checker and generator\n[CHECKER] Author: Noah Serapilio)\n-------------------------------------\nSettings\n-------------------------------------\n[CHECKER] Debug Mode: ${debug}\n[CHECKER] Using Socks5 Proxies: ${socks5}\n[CHECKER] Auto-Gen: ${autogen}\n[CHECKER] No Error Messages: ${noerrormessages}\n[CHECKER] Auto Grab Proxies: ${autograbproxy}\n-------------------------------------\n`))
+console.log(colors.white(`\n-------------------------------------\n[CHECKER] Discord Nitro Code Checker\n[CHECKER] Author: loading8009#0827 (spicyness\'s alt)\n-------------------------------------\nSettings\n-------------------------------------\n[CHECKER] Debug Mode: ${debug}\n[CHECKER] Using Socks5 Proxies: ${socks5}\n[CHECKER] Auto-Gen: ${autogen}\n[CHECKER] No Error Messages: ${noerrormessages}\n[CHECKER] Auto Grab Proxies: ${autograbproxy}\n-------------------------------------\n`))
 
 getGiftCode = function () {
     let code = '';
@@ -119,13 +119,13 @@ request.get({
     try {
             body = JSON.parse(body);
             if(body.message != "Unknown Gift Code" && body.message != "You are being rate limited." && body.message != "404: Not Found"){
-                console.log(colors.green(`[CHECKER]AYYY, Code Found, You lucky MF!: https://discord.gift/${code}`));
+                console.log(colors.green(`[CHECKER] Code Found!: https://discord.gift/${code}`));
                 fs.appendFileSync("working.txt", code+'\n', "UTF-8",{'flags': 'a+'});
                 i++
                 a++
             }
             else {
-                console.log(colors.red(`${code} Doesnt work!` + colors.white(`${debug ? `\n[DEBUG] Proxy: ${proxy}\n[DEBUG] Message: ${body.message}` : ''}`)));
+                console.log(colors.red(`${code} is invalid!` + colors.white(`${debug ? `\n[DEBUG] Proxy: ${proxy}\n[DEBUG] Message: ${body.message}` : ''}`)));
                 i++
             }
         }
@@ -166,7 +166,7 @@ request.get({
                 a++
             }
             else {
-                console.log(colors.red(`${code} Doesnt Work!` + colors.white(`${debug ? `\n[DEBUG] Proxy: ${proxy}\n[DEBUG] Message: ${body.message}` : ''}`)));
+                console.log(colors.red(`${code} is invalid!` + colors.white(`${debug ? `\n[DEBUG] Proxy: ${proxy}\n[DEBUG] Message: ${body.message}` : ''}`)));
                 i++
             }
         }
@@ -197,7 +197,7 @@ request.get({
                 i++
             }
             else {
-                console.log(colors.red(`${code} Doesnt Work!` + colors.white(`${debug ? `\n[DEBUG] Proxy: ${proxy}\n[DEBUG] Message: ${body.message}` : ''}`)));
+                console.log(colors.red(`${code} is invalid!` + colors.white(`${debug ? `\n[DEBUG] Proxy: ${proxy}\n[DEBUG] Message: ${body.message}` : ''}`)));
                 i++
             }
         }
